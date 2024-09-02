a5e75a817de0341cdcdfba1d8a589483bb425d6a
tylergeorges <tmg320v@gmail.com>
2024-09-02 17:57:30 -0400

    refactor: update showcase

--- src/components/reactive-ascii/index.tsx
+++ src/components/reactive-ascii/index.tsx
@@ -1,15 +1,21 @@
 'use client';
 
 import { useReactiveAscii } from '@/components/reactive-ascii/use-reactive-ascii';
+import { cn } from '@/lib/utils';
 import React from 'react';
 
 interface ReactiveAsciiProps {
   children: string;
   fps?: number;
+  className?: string;
 }
 
-export const ReactiveAscii = ({ children: asciiText, fps = 12 }: ReactiveAsciiProps) => {
+export const ReactiveAscii = ({ children: asciiText, className, fps = 12 }: ReactiveAsciiProps) => {
   const reactiveAsciiRef = useReactiveAscii({ asciiText, fps });
 
-  return <pre ref={reactiveAsciiRef}>&nbsp;</pre>;
+  return (
+    <pre ref={reactiveAsciiRef} className={cn('bg-transparent font-pixels', className)}>
+      {asciiText}
+    </pre>
+  );
 };
--- src/components/showcase.tsx
+++ src/components/showcase.tsx
@@ -38,11 +38,28 @@ export const Showcase = () => {
       </div>
 
       <div className="gap-[25px] center vertical">
-        <div className="size-11 rounded-md bg-secondary md:size-14 md:rounded-[15px]" />
+        <img
+          src="https://widgetbot.io/widgetbot_blurple.png"
+          className="m-0 size-11 rounded-md md:size-14 md:rounded-[15px]"
+        />
 
-        <div className="text-center">
-          <h2 className="m-0 text-[22px] font-black">WidgetBot</h2>
-          <p className="m-0 text-base">frontend developer</p>
+        <div className="gap-2.5 text-center vertical">
+          <h2 className="m-0 text-base font-black">WidgetBot</h2>
+          <p className="m-0 text-base">
+            Refactored the outdated codebase with a new architecture utilizing GraphQl, Next.js,
+            TypeScript, and reusable React Functional components.
+          </p>
+        </div>
+
+        <div className="gap-[15px] vertical">
+          <img
+            src="https://pbs.twimg.com/media/ETNbEkKU4AA6rM7.jpg"
+            className="m-0 size-full rounded-[10px] object-cover"
+          />
+          <img
+            src="https://blog.widgetbot.io/content/images/size/w600/2023/07/discord-widgetbot-emerald-sub.png"
+            className="m-0 size-full rounded-[10px] object-cover"
+          />
         </div>
       </div>
     </div>
